813163889417feb1b4e1bc10ef547c58882579ad
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-08-10 18:57:32 +0700

    app/feat: make categorizing work

--- packages/app/src/lib/components/pie-chart.svelte
+++ packages/app/src/lib/components/pie-chart.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-import { EditIcon } from "@lucide/svelte";
+import { EditIcon, PlusIcon, TrashIcon } from "@lucide/svelte";
 import { Arc, PieChart, Text } from "layerchart";
 import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
 import { Button, buttonVariants } from "$lib/components/ui/button/index.js";
 import * as Card from "$lib/components/ui/card/index.js";
 import * as Chart from "$lib/components/ui/chart/index.js";
 import * as Popover from "$lib/components/ui/popover/index.js";
+import * as Table from "$lib/components/ui/table/index.js";
 
 const chartData = [
   { browser: "chrome", visitors: 275, color: "var(--color-chrome)" },
@@ -25,63 +26,79 @@ const chartConfig = {
 } satisfies Chart.ChartConfig;
 </script>
 
-<Popover.Root>
-    <Card.Root class="flex flex-col">
-        <Card.Header class="items-center">
-            <Card.Title>Expense by category</Card.Title>
-        </Card.Header>
-        <Card.Content class="flex-1">
-            <Chart.Container
-                config={chartConfig}
-                class="mx-auto aspect-square max-h-[250px]"
+<Card.Root class="flex flex-col">
+    <Card.Header class="items-center text-center">
+        <Card.Title>Expense by category</Card.Title>
+    </Card.Header>
+    <Card.Content class="flex-1">
+        <Chart.Container
+            config={chartConfig}
+            class="mx-auto aspect-square max-h-[250px]"
+        >
+            <PieChart
+                data={chartData}
+                key="browser"
+                value="visitors"
+                cRange={chartData.map((d) => d.color)}
+                c="color"
+                props={{
+                  pie: {
+                    motion: "tween",
+                  },
+                }}
             >
-                <PieChart
-                    data={chartData}
-                    key="browser"
-                    value="visitors"
-                    cRange={chartData.map((d) => d.color)}
-                    c="color"
-                    props={{
-                      pie: {
-                        motion: "tween",
-                      },
-                    }}
-                >
-                    {#snippet tooltip()}
-                        <Chart.Tooltip hideLabel />
-                    {/snippet}
-                    {#snippet arc({ props, visibleData, index })}
-                        {@const browser = visibleData[index].browser}
-                        <Arc {...props}>
-                            {#snippet children({ getArcTextProps })}
-                                <Text
-                                    value={browser}
-                                    {...getArcTextProps("centroid")}
-                                    font-size="12"
-                                    class="fill-background capitalize"
-                                />
-                            {/snippet}
-                        </Arc>
-                    {/snippet}
-                </PieChart>
-            </Chart.Container>
-            <div>Add current categories here</div>
-        </Card.Content>
-        <Card.Footer class="flex-col gap-2 text-sm">
-            <div class="flex items-center gap-2 font-medium leading-none">
-                <Popover.Trigger>
-                    <Button variant="secondary">
-                        <EditIcon />
-                        Edit Category
-                    </Button>
-                </Popover.Trigger>
-            </div>
-        </Card.Footer>
-    </Card.Root>
-
-    <Popover.Content
-        interactOutsideBehavior="close"
-    >
-        table here
-    </Popover.Content>
-</Popover.Root>
+                {#snippet tooltip()}
+                    <Chart.Tooltip hideLabel />
+                {/snippet}
+                {#snippet arc({ props, visibleData, index })}
+                    {@const browser = visibleData[index].browser}
+                    <Arc {...props}>
+                        {#snippet children({ getArcTextProps })}
+                            <Text
+                                value={browser}
+                                {...getArcTextProps("centroid")}
+                                font-size="12"
+                                class="fill-background capitalize"
+                            />
+                        {/snippet}
+                    </Arc>
+                {/snippet}
+            </PieChart>
+        </Chart.Container>
+        <div class="mt-6">
+            <Table.Root>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.Head>Name</Table.Head>
+                        <Table.Head>Amount</Table.Head>
+                        <Table.Head>Actions</Table.Head>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {#each chartData as item, index}
+                        <Table.Row>
+                            <Table.Cell>{item.browser}</Table.Cell>
+                            <Table.Cell>{item.visitors}</Table.Cell>
+                            <Table.Cell>
+                                <Button variant="secondary" size="icon">
+                                    <EditIcon />
+                                </Button>
+                                <Button variant="secondary" size="icon">
+                                    <TrashIcon />
+                                </Button>
+                            </Table.Cell>
+                        </Table.Row>
+                    {/each}
+                </Table.Body>
+            </Table.Root>
+        </div>
+    </Card.Content>
+    <Card.Footer class="flex-col gap-2 text-sm">
+        <div class="flex items-center gap-2 font-medium leading-none">
+            <Button variant="secondary">
+                <PlusIcon />
+                Add category
+            </Button>
+        </div>
+    </Card.Footer>
+</Card.Root>
