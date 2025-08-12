fd0e20ce4a451c43bab63adc815996cf99d14d0e
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-08-12 15:00:37 +0700

    app-v2/feat: split to `/` and `/overview`

---
+++ packages/app-v2/src/routes/overview/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+import { Chart } from "chart.js/auto";
+import { onMount } from "svelte";
+
+let pieChart: HTMLCanvasElement;
+// Chart.register(PieController, ArcElement);
+
+onMount(() => {
+  const data = [
+    { name: "Entertainment", value: 10 },
+    { name: "Food", value: 20 },
+    { name: "Cats", value: 20 },
+    { name: "Others", value: 15 },
+  ];
+
+  new Chart(pieChart, {
+    type: "pie",
+    data: {
+      labels: data.map((row) => row.name),
+      datasets: [
+        {
+          label: "Category",
+          data: data.map((row) => row.value),
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          position: "bottom",
+          labels: {
+            padding: 40,
+          },
+        },
+      },
+    },
+  });
+});
+</script>
+
+<div class="flex flex-col gap-4">
+    <span class="font-bold text-xl">Overview</span>
+    <canvas
+        bind:this={pieChart}
+        class="max-h-[40em] pt-2"
+    ></canvas>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Total Income</th>
+            <th class="text-center">Total Expense</th>
+            <th class="text-right">Current Balance</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>3,432,321 VND</td>
+            <td class="text-center">2,432,321 VND</td>
+            <td class="text-right">1,987,321 VND</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <a class="underline" href="/">Back</a>
+</div>
