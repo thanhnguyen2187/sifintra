2b63ef1b274f554b59caaf1b571f2e363d16dfc7
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-08-13 15:37:21 +0700

    backend/feat: add struct of SePay hoook

--- packages/backend/src/handlers.rs
+++ packages/backend/src/handlers.rs
@@ -1,6 +1,24 @@
 use axum::Json;
+use serde_derive::{Deserialize, Serialize};
 use serde_json::{Value, json};
 
-pub async fn handle_hook_sepay() -> Json<Value> {
-    Json(json!({ "success": true }))
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Payload {
+    pub gateway: String,
+    pub transaction_date: String,
+    pub account_number: String,
+    pub sub_account: Value,
+    pub code: Value,
+    pub content: String,
+    pub transfer_type: String,
+    pub description: String,
+    pub transfer_amount: i64,
+    pub reference_code: String,
+    pub accumulated: i64,
+    pub id: i64,
+}
+
+pub async fn handle_hook_sepay(Json(payload): Json<Payload>) -> Json<Value> {
+    Json(json!(payload))
 }
