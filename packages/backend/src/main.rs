f0b6ad508942dc7ba77b08c8794919274cc59acc
Thanh Nguyen <thanhnguyen2187@gmail.com>
2025-08-15 00:34:04 +0700

    backend/feat: add tracing

--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -9,13 +9,39 @@ use crate::app_state::AppState;
 use crate::db::establish_connection;
 use crate::err::Result;
 use crate::handlers::{handle_hook_sepay, handle_stats};
-use axum::{Router, routing::get, routing::post};
+use axum::{
+    Router,
+    body::Bytes,
+    extract::MatchedPath,
+    http::{HeaderMap, Request},
+    response::{Html, Response},
+    routing::{get, post},
+};
 use dotenvy::dotenv;
 use std::sync::{Arc, Mutex};
+use tower_http::trace::TraceLayer;
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                format!(
+                    "{}=debug,tower_http=debug,axum::rejection=trace",
+                    env!("CARGO_CRATE_NAME")
+                )
+                .into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let conn = establish_connection()?;
     let shared_state = Arc::new(Mutex::new(AppState { conn }));
     let app = Router::new()
@@ -23,10 +49,11 @@ async fn main() -> Result<()> {
         .route("/api/v1/hooks/sepay", post(handle_hook_sepay))
         .route("/api/v1/stats", get(handle_stats))
         .fallback(frontend::static_handler)
+        .layer(TraceLayer::new_for_http())
         .with_state(shared_state);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
 
-    println!("Listening on http://127.0.0.1:3000");
+    info!("Listening on http://127.0.0.1:3000");
     axum::serve(listener, app).await?;
     Ok(())
 }
